fa635b659678304b878d295dfd9a49540347cdad
Denny Cao <8008925@philasd.org>
2022-09-02 14:37:08 -0400

    initial commit

---
+++ Hello.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.*;
+
+public class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double perimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+
+    public double area() {
+        double semiPerm = perimeter() / 2;
+
+        return Math.sqrt(semiPerm * (semiPerm - this.side1) * (semiPerm - this.side2) * (semiPerm - this.side3));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter length of side 1: ");
+        double side1 = scanner.nextDouble();
+        System.out.println("Enter length of side 2: ");
+        double side2 = scanner.nextDouble();
+        System.out.println("Enter length of side 3: ");
+        double side3 = scanner.nextDouble();
+
+         Triangle tri = new Triangle(side1, side2, side3);
+
+        System.out.println(tri.area());
+    }
+}
+ 
