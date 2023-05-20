64e6015dc1d1382e31877068530f5d3105ffd836
Denny Cao <dennycaopersonal@gmail.com>
2023-05-20 00:10:37 -0400

    finish knight

--- projects/Chess/src/com/chessgame/Game/Game.java
+++ projects/Chess/src/com/chessgame/Game/Game.java
@@ -37,4 +37,97 @@ public class Game {
             blackGraveyard.add(piece);
         }
     }
+
+    /**
+     * Method to check if the king is in check
+     * @param board Piece[][] array representing the board
+     * @param isWhite boolean value to determine if the king is white or black
+     * @return boolean value to determine if the king is in check
+     */
+    public boolean isCheck(Piece[][] board, boolean isWhite) {
+        // Find the king
+        int[] kingPos = new int[2];
+        for (int i = 0; i < Board.ROWS; i++) {
+            for (int j = 0; j < Board.COLUMNS; j++) {
+                if (board[i][j] instanceof King && board[i][j].getIsWhite() == isWhite) {
+                    kingPos[0] = i;
+                    kingPos[1] = j;
+                }
+            }
+        }
+
+        // Check if the king is in check
+        for (int i = 0; i < Board.ROWS; i++) {
+            for (int j = 0; j < Board.COLUMNS; j++) {
+                if (board[i][j] != null && board[i][j].getIsWhite() != isWhite && board[i][j].isLegalMove(board, new int[]{i, j}, kingPos)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Method to check if the king is in checkmate
+     * @param board Piece[][] array representing the board
+     * @param isWhite boolean value to determine if the king is white or black
+     * @return boolean value to determine if the king is in checkmate
+     */
+    public boolean isCheckmate(Piece[][] board, boolean isWhite) {
+        // Find the king
+        int[] kingPos = new int[2];
+        for (int i = 0; i < Board.ROWS; i++) {
+            for (int j = 0; j < Board.COLUMNS; j++) {
+                if (board[i][j] instanceof King && board[i][j].getIsWhite() == isWhite) {
+                    kingPos[0] = i;
+                    kingPos[1] = j;
+                }
+            }
+        }
+
+        // Check if the king is in checkmate
+        if (isCheck(board, isWhite)) {
+            for (int i = kingPos[0] - 1; i <= kingPos[0] + 1; i++) {
+                for (int j = kingPos[1] - 1; j <= kingPos[1] + 1; j++) {
+                    if (i >= 0 && i < Board.ROWS && j >= 0 && j < Board.COLUMNS && board[i][j] == null && !isCheck(board, isWhite)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method to check if the game is a stalemate
+     * @param board Piece[][] array representing the board
+     * @param isWhite boolean value to determine if the king is white or black
+     * @return boolean value to determine if the game is a stalemate
+     */
+    public boolean isStalemate(Piece[][] board, boolean isWhite) {
+        // Find the king
+        int[] kingPos = new int[2];
+        for (int i = 0; i < Board.ROWS; i++) {
+            for (int j = 0; j < Board.COLUMNS; j++) {
+                if (board[i][j] instanceof King && board[i][j].getIsWhite() == isWhite) {
+                    kingPos[0] = i;
+                    kingPos[1] = j;
+                }
+            }
+        }
+
+        // Check if the game is a stalemate
+        if (!isCheck(board, isWhite)) {
+            for (int i = kingPos[0] - 1; i <= kingPos[0] + 1; i++) {
+                for (int j = kingPos[1] - 1; j <= kingPos[1] + 1; j++) {
+                    if (i >= 0 && i < Board.ROWS && j >= 0 && j < Board.COLUMNS && board[i][j] == null) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
